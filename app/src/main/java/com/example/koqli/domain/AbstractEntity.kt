d43804694b3fe3d8f4771ffd32c3201854e3a086
Asato Imanishi <dtm.music.producer@gmail.com>
2017-10-03 00:35:08 +0900

    警告を消した

--- app/src/main/java/com/example/koqli/domain/AbstractEntity.kt
+++ app/src/main/java/com/example/koqli/domain/AbstractEntity.kt
@@ -9,7 +9,8 @@ import org.apache.commons.lang3.SerializationUtils
 abstract class AbstractEntity<I: Identity<*>, E: Entity<I, E>>(override val identity: I): Entity<I, E>{
     override fun isSame(entity: Entity<I, E>): Boolean = entity.identity == identity
 
-    override fun equals(other: E): Boolean = this == other || (this is Entity<I,E> && isSame(other))
+    override fun equals(other: E): Boolean = this == other || isSame(other)
 
+    @Suppress("UNCHECKED_CAST")
     override fun clone(): E = SerializationUtils.clone(this) as E
 }
